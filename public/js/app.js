dddc2aec568922030e97ff82161575b4ab0d0df0
Matt Lane <lane.matthew@gmail.com>
2016-01-06 09:43:57 -0800

    finish webscraping

---
+++ public/js/app.js
@@ -0,0 +1,59 @@
+$(function() {
+
+  var $movieForm = $(".movie-form");
+  var $movieArea = $(".jumbotron");
+  var $compareMovies = $("#compare-movies");
+  var $movie1Weekly = $("#movie1-weekly");
+  var $movie2Weekly = $("#movie2-weekly");
+  var $movie1Title = $("#movie1-title");
+  var $movie2Title = $("#movie2-title");
+  var movies = [{
+    title: '',
+    urlTitle: ''
+  }, {
+    title: '',
+    urlTitle: ''
+  }];
+
+  $movieForm.on("submit", function(e) {
+    e.preventDefault();
+    var $form = $(this);
+    var movieData = $form.find('input').val();
+    var $results = $form.parent().siblings().eq(0);
+    var idx = $form.data('idx');
+
+    $.get("/movie?movie=" + encodeURIComponent(movieData), function(res) {
+      $results.empty();
+      if (res.err) { return $results.append(res.err); }
+      $results.append("<div>Please select your film:</div>");
+      res.forEach(function(movie) {
+        $results.append("<div><input type='radio' name='movie"+idx+"' value='"+movie.urlTitle+"'>"+movie.title+"</div>");
+      });
+    });
+  });
+
+  $movieArea.on("click", "input[type='radio']", function() {
+    var idx = $(this).parents(".jumbotron").find("form").data("idx");
+    movies[idx].urlTitle = this.value;
+    movies[idx].title = $(this).parent().text();
+  });
+
+  $compareMovies.on("click", function() {
+    if (movies[0].urlTitle && movies[1].urlTitle) {
+      $.get("/movies?movie1=" + encodeURIComponent(movies[0].urlTitle) + "&movie2=" + encodeURIComponent(movies[1].urlTitle), function(res) {
+        $movie1Weekly.empty();
+        $movie2Weekly.empty();
+        $movie1Title.text(movies[0].title);
+        $movie2Title.text(movies[1].title);
+        res.movie1.forEach(function(week) {
+          $movie1Weekly.append("<li>"+week+"</li>");
+        });
+        res.movie2.forEach(function(week) {
+          $movie2Weekly.append("<li>"+week+"</li>");
+        });
+      });
+    }
+    
+  });
+
+});
--- server.js
+++ server.js
@@ -11,6 +11,56 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/movie', function(req, res) {
+  var url = 'http://www.boxofficemojo.com/search/?q=' + req.query.movie;
+
+  request(url, function(error, response, html) {
+    var $ = cheerio.load(html);
+    var body = $("#body");
+    body.find('br').remove();
+    var resultSummary = body.find('form').next();
+    if ((/no movies/gi).test(resultSummary.text())) {
+      res.send({err: "Sorry, I didn't find any movies. Try again?"});
+    } else {
+      var data = [];
+      resultSummary.next().children().slice(1).each(function(idx,row) {
+        var movieInfo = $(row).children().eq(0).find('font a').eq(0);
+        data.push({
+          title: movieInfo.text(), 
+          urlTitle: movieInfo[0].attribs.href.split("=")[1]
+        });
+      });
+      res.send(data);
+    }
+  });
+});
+
+app.get('/movies', function(req, res) {
+  var url1 = 'http://www.boxofficemojo.com/movies/?page=weekly&id=' + req.query.movie1;
+  var url2 = 'http://www.boxofficemojo.com/movies/?page=weekly&id=' + req.query.movie2;
+
+  function grabWeeklyData(html) {
+    var weeklies = [];
+    var $ = cheerio.load(html);
+    var tables = $(".chart-wide");
+    tables.each(function(idx, table) {
+      $(table).children().slice(1).each(function(idx, row) {
+        weeklies.push($(row).children().eq(2).text());
+      });
+    });
+    return weeklies;
+  }
+
+  request(url1, function(error, response, html) {
+    var data = {};
+    data.movie1 = grabWeeklyData(html);
+    request(url2, function(error, response, html) {
+      data.movie2 = grabWeeklyData(html);
+      res.send(data);
+    });
+  });
+});
+
 app.listen(3002, function() {
   console.log("Listening on localhost:3002");
 });
